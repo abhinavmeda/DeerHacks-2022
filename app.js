b19c9782c8b9e62ff051b0f328c1b9cf927c7ee8
abhinavmeda <39966275+abhinavmeda@users.noreply.github.com>
2022-05-01 00:58:29 -0400

    got images to load

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
-function setup(){
-    background(255, 0, 0);
-    document.getElementById('take_photo').addEventListener('click', function(){
-        alert('clicked');
-    });
-}
+
+let camera_button = document.querySelector("#start-camera");
+let video = document.querySelector("#video");
+
+camera_button.addEventListener('click', async function() {
+	const camera_constraints = { video: true, audio: false };
+   	stream = await navigator.mediaDevices.getUserMedia(camera_constraints);
+	video.srcObject = stream;
+
+	const newButton = document.createElement("button");
+	newButton.innerHTML = "click";
+	const canvas = document.createElement("canvas");
+	canvas.height = 240;
+	canvas.width = 320;
+	document.getElementById('display').appendChild(newButton);
+	newButton.addEventListener('click', () => {
+		canvas.getContext("2d").drawImage(video, 0, 0, canvas.width, canvas.height);
+		video.remove();
+		newButton.remove();
+		stream.getTracks().forEach(function(track) {
+			track.stop();
+		});
+		document.getElementById('display').appendChild(canvas);
+		var img = canvas.toDataURL("image/png");
+		console.log(img);
+		camera_button.remove();
+	})
+});
